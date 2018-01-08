db5e1596255f5a5b71d0c4aa564182ef34fb5d9a
Mark Bui <tvyybui@yahoo.com>
2018-01-07 18:57:21 -0800

    finished one solution to reversing a number

--- reverseInt/reverseInt.js
+++ reverseInt/reverseInt.js
@@ -8,3 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+function reverseInt(int) {
+  console.log('number to reverse: ' + int)
+  let num = '';
+  let reverseNum = '';
+  num = int.toString();
+  for (var i = num.length - 1; i >= 0; i--) {
+    reverseNum += num[i];
+  }
+  console.log('reversed number: ' + reverseNum);
+}
+
+
+reverseInt(123);
+reverseInt(38);
