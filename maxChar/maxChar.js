7ebc95c07bfbd67c8d20595f56936655b489bcb7
Mark Bui <tvyybui@yahoo.com>
2018-01-10 22:02:56 -0800

    got first part of max character working, saving characters into an object

--- maxChar/maxChar.js
+++ maxChar/maxChar.js
@@ -7,4 +7,25 @@
 
 function maxChar(str) {
 
-}
+    var strObj = {};
+    var splitStr = str.split('');
+    splitStr.forEach(function(el) {
+        if (strObj[el]){
+            strObj[el]++;
+        } else {
+            strObj[el] = 1;
+        }
+    });
+
+    var max = 0;
+    for (var test in strObj) {
+        if ((strObj[test]) > max) {
+            max = strObj;
+            console.log(test);
+            return test;
+        };
+    };
+};
+
+maxChar("abcccccccd")
+// maxChar("apple 1231111")
