41c329afd5d00620209c547a10b711b6cd4d381a
Mark Bui <tvyybui@yahoo.com>
2018-01-12 11:09:21 -0800

    added similar questions to the top of the file for reference

--- maxChar/maxChar.js
+++ maxChar/maxChar.js
@@ -1,6 +1,13 @@
 // --- Directions
+// these questions are the same:
 // Given a string, return the character that is most
 // commonly used in the string.
+// What is the most common character in a string?
+// Does the given string have any repeated characters in it?
+
+// this question is not quite the same but uses similar logic:
+// Does string A have the same characters as string B (is it an anagram)?
+
 // --- Examples
 // maxChar("aabcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
