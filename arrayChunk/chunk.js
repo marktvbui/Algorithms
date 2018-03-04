ce06f1bac82f202ce485ffb348785063a7fcd3c3
Mark Bui <tvyybui@yahoo.com>
2018-03-03 19:23:12 -0800

    solution to arraychunk, starting anagrams

--- arrayChunk/chunk.js
+++ arrayChunk/chunk.js
@@ -8,35 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-    // use splice:
-    // var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-    // var removed = myFish.splice(3, 1);
-    // removed is ["mandarin"]
-    // myFish is ["angel", "clown", "drum", "sturgeon"]
-    let newArry = [];
-    function chunkArry() {
-        let tempArry = [];
-        console.log(array.length);
-        if (array.length > 0) {
-            console.log('this is working');
-            for (let i = 0; i < size; i++) {
-                tempArry.push(array[i]);
-            };
-        newArry.push(tempArry);
-        console.log(newArry);
-        if (size > 0) {
-            array.splice(0, size);
-            console.log('new array: ' + array);
-        }
-        chunkArry();
-        }
-    };
-    if (array.length > 0) {
-        chunkArry();
-    }
-};
+// function chunk(array, size) {
+//     // use splice:
+//     // var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+//     // var removed = myFish.splice(3, 1);
+//     // removed is ["mandarin"]
+//     // myFish is ["angel", "clown", "drum", "sturgeon"]
+//     let newArry = [];
+//     function chunkArry() {
+//         let tempArry = [];
+//         console.log(array.length);
+//         if (array.length > 0) {
+//             console.log('this is working');
+//             for (let i = 0; i < size; i++) {
+//                 tempArry.push(array[i]);
+//             };
+//         newArry.push(tempArry);
+//         console.log(newArry);
+//         if (size > 0) {
+//             array.splice(0, size);
+//             console.log('new array: ' + array);
+//         }
+//         chunkArry();
+//         }
+//     };
+//     if (array.length > 0) {
+//         chunkArry();
+//     }
+// };
 
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      console.log('index: ' + index);
+      console.log(chunked);
+      index += size;
+    }
+
+    console.log(chunked);
+}
 
 chunk([1,2,3,4,5,6,7,8,9,10,11,12,13], 3);
-// module.exports = chunk;
